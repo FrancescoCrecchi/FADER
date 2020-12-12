df8e625014e5ab63f525098cb4d94ee9d149cea7
Francesco Crecchi <francesco.crecchi@di.unipi.it>
2020-12-12 16:35:39 +0100

    tSNE Reject CIFAR10 v0.1

---
+++ cifar10/tsne_rej_visualize.py
@@ -0,0 +1,63 @@
+from secml.adv.seceval import CSecEvalData
+from secml.array import CArray
+from secml.data import CDataset
+from secml.figure import CFigure
+from secml.ml.classifiers.reject import CClassifierRejectThreshold
+
+# GAMMA = 1000
+EPS = 3.0
+
+if __name__ == '__main__':
+    # 1. Load clf_rej
+    # TSNE -> MINMAX -> KDE
+    # clf_rej = CClassifierRejectThreshold.load('tsne_rej_test_gamma_{:}.gz'.format(GAMMA))
+    # clf_rej = CClassifierRejectThreshold.load('/home/crecchi/DNR/mnist/tsne_rej/tsne_rej_test_gamma/tsne_rej_test_gamma_10000.0.gz')
+    # clf_rej = CClassifierRejectThreshold.load('tsne_rej.gz')
+    ptsne = clf_rej.preprocess
+
+    # # TSNE -> SVM
+    # clf_rej = CClassifierRejectThreshold.load('tsne_nr.gz')
+    # ptsne = clf_rej.clf.preprocess.preprocess
+
+    # # 2. Load attack points
+    # seval_data = CSecEvalData.load('tsne_rej_bb_seval_it_0.gz')
+    #
+    # # Fwd through CReducerTSNE
+    # eps_idx = seval_data.param_values.tolist().index(EPS)
+    # X_adv, Y_rej = seval_data.adv_ds[eps_idx].X, seval_data.Y_pred[eps_idx]
+    # X_2d = ptsne.forward(X_adv)
+    # adv_ds_2d = CDataset(X_2d, Y_rej)
+
+    # # TRAINING DATA
+    # from mnist.fit_dnn import get_datasets
+    # random_state = 999
+    # N_TRAIN, N_TEST = 10000, 1000
+    # _, vl, _ = get_datasets(random_state)
+    # tr_idxs = CArray.randsample(vl.X.shape[0], shape=N_TRAIN, random_state=random_state)
+    # tr_sample = vl[tr_idxs, :]
+    X_2d = ptsne.forward(tr_sample.X)
+    adv_ds_2d = CDataset(X_2d, tr_sample.Y)
+
+    # Plot points together with decision regions
+    fig = CFigure(12, 10)
+
+    # HACK: Detach from preprocess to plot
+    preproc = clf_rej.preprocess.copy()
+    clf_rej.preprocess = None
+    # preproc = clf_rej.clf.preprocess.preprocess.copy()
+    # clf_rej.clf.preprocess.preprocess = None
+
+    fig.sp.plot_decision_regions(clf_rej,
+                                 grid_limits=[[-130, 130], [-130, 130]],
+                                 # levels=CArray.arange(0.5, clf_rej.n_classes).tolist(),
+                                 alpha=1.0,
+                                 # colorbar=True,
+                                 n_grid_points=100)
+    fig.sp.plot_ds(adv_ds_2d)
+
+    clf_rej.preprocess = preproc
+    # clf_rej.clf.preprocess.preprocess = preproc
+
+    # Dump to disk
+    fig.savefig('tsne_rej_visualize')
+    # fig.savefig('tsne_nr_visualize')
--- cifar10/tsne_reject.py
+++ cifar10/tsne_reject.py
@@ -31,9 +31,11 @@ if __name__ == '__main__':
     # Create layer_classifier
     feat_extr = CNormalizerDNN(dnn, out_layer='features:29')
     # Compose classifier
-    tsne = CReducerPTSNE(epochs=250, batch_size=128, random_state=random_state, preprocess=feat_extr)
-    nmz = CNormalizerMinMax(preprocess=tsne)
-    clf = CClassifierMulticlassOVA(classifier=CClassifierKDE, kernel=CKernelRBF(), preprocess=nmz)
+    tsne = CReducerPTSNE(epochs=1000,
+                         batch_size=32,
+                         preprocess=feat_extr,
+                         random_state=random_state)
+    clf = CClassifierMulticlassOVA(classifier=CClassifierKDE, kernel=CKernelRBF(), preprocess=tsne)
 
     # Select 10K training data and 1K test data (sampling)
     tr_idxs = CArray.randsample(vl.X.shape[0], shape=N_TRAIN, random_state=random_state)
@@ -41,48 +43,60 @@ if __name__ == '__main__':
     ts_idxs = CArray.randsample(ts.X.shape[0], shape=N_TEST, random_state=random_state)
     ts_sample = ts[ts_idxs, :]
 
-    # Xval
-    def compute_hiddens(n_hiddens, n_layers):
-        return sum([[[l] * k for l in n_hiddens] for k in range(1, n_layers+1)], [])
+    # # Xval
+    # def compute_hiddens(n_hiddens, n_layers):
+    #     return sum([[[l] * k for l in n_hiddens] for k in range(1, n_layers+1)], [])
+    #
+    # xval_params = {
+    #     'preprocess.preprocess.n_hiddens': compute_hiddens([64, 256], 2),
+    #     'kernel.gamma': [1e-3, 1, 100, 1000]
+    # }
+    #
+    # # Let's create a 3-Fold data splitter
+    # xval_splitter = CDataSplitterKFold(num_folds=3, random_state=random_state)
+    #
+    # # Parallel?
+    # clf.n_jobs = 8
+    #
+    # # Select and set the best training parameters for the classifier
+    # clf.verbose = 1
+    # print("Estimating the best training parameters...")
+    # best_params = clf.estimate_parameters(
+    #     dataset=tr_sample,
+    #     parameters=xval_params,
+    #     splitter=xval_splitter,
+    #     metric='accuracy',
+    #     perf_evaluator='xval'
+    # )
+    # clf.verbose = 0
+    #
+    # print("The best training parameters are: ",
+    #       [(k, best_params[k]) for k in sorted(best_params)])
 
-    xval_params = {
-        'preprocess.preprocess.n_hiddens': compute_hiddens([64, 256], 2),
-        'kernel.gamma': [1e-3, 1, 100, 1000]
-    }
-
-    # Let's create a 3-Fold data splitter
-    xval_splitter = CDataSplitterKFold(num_folds=3, random_state=random_state)
-
-    # Parallel?
-    clf.n_jobs = 8
-
-    # Select and set the best training parameters for the classifier
-    clf.verbose = 1
-    print("Estimating the best training parameters...")
-    best_params = clf.estimate_parameters(
-        dataset=tr_sample,
-        parameters=xval_params,
-        splitter=xval_splitter,
-        metric='accuracy',
-        perf_evaluator='xval'
+    # Setting best params (external xval)
+    clf.set_params({
+        'kernel.gamma': 10,
+        'preprocess.n_hiddens': [256, 256]}
     )
-    clf.verbose = 0
-
-    print("The best training parameters are: ",
-          [(k, best_params[k]) for k in sorted(best_params)])
+    # Expected performance: 0.9735
 
     # We can now create a classifier with reject
     clf.preprocess = None  # TODO: "preprocess should be passed to outer classifier..."
-    clf_rej = CClassifierRejectThreshold(clf, 0., preprocess=nmz)
+    clf_rej = CClassifierRejectThreshold(clf, -1000., preprocess=tsne)
+
     # We can now fit the clf_rej
+    clf_rej.preprocess.verbose = 1  # DEBUG
     clf_rej.fit(tr_sample.X, tr_sample.Y)
+
     # Set threshold (FPR: 10%)
-    # TODO: "..and set the rejection threshold for (D)NR to reject 10% of the samples when no attack is performed
     clf_rej.threshold = clf_rej.compute_threshold(0.1, ts_sample)
 
     # Check test performance
     y_pred = clf_rej.predict(ts.X, return_decision_function=False)
-    acc = CMetricAccuracy().performance_score(ts.Y, y_pred)
+
+    from secml.ml.peval.metrics import CMetric, CMetricAccuracy
+
+    acc = CMetric.create('accuracy').performance_score(ts.Y, y_pred)
     print("Model Accuracy: {}".format(acc))
 
     # Dump to disk
